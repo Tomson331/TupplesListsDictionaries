4e0349e3c902c9a1a2f8639e40e4efa4d7043e1b
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-08-03 19:43:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# Lesson 5
+# Chapter 8 - Conditional Logic and Control Flow
+# 8.1 Compare Values
+type(True)
+<class 'bool'>
+
+type(False)
+<class 'bool'>
+1 == 1
+3 > 5
+
+"a" == "a"
+"a" == "b"
+"a" < "b"
+"a" > "b"
+
+"apple" < "astronaut"
+"beauty" > "truth"
+
+# 8.1 - Compare Values
+# Solutions to review exercises
+
+
+# Exercise 1
+# Test whether these expressions are True or False
+
+print(1 <= 1)
+print(1 != 1)
+print(1 != 2)
+print("good" != "bad")
+print("good" != "Good")
+print(123 == "123")
+
+# Exercise 2
+# Fill in the blank so that each of the following expressions are True
+
+# 3 __ 4
+# Any of the following:
+3 < 4
+3 <= 4
+3 != 4
+
+# 10 __ 5
+# Any of the following:
+10 > 5
+10 >= 5
+10 != 5
+
+# "jack" __ "jill"
+# Any of the following:
+"jack" < "jill"
+"jack" <= "jill"
+"jack" != "jill"
+
+# 42 __ "42"
+42 != "42"
